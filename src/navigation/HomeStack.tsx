838f64c367c784b3a7b8bb6656757817405db561
KhatriSahil2799 <khatrisahil2799@gmail.com>
2025-01-16 00:18:33 +0530

    Implemented home Stack

--- src/navigation/HomeStack.tsx
+++ src/navigation/HomeStack.tsx
@@ -3,14 +3,15 @@ import React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import Screen1 from "../screens/Screen1";
 import Screen2 from "../screens/Screen2";
+import { HomeStackParamList } from "../utils/types";
 
-const Stack = createNativeStackNavigator();
+const Stack = createNativeStackNavigator<HomeStackParamList>();
 
 function HomeStack() {
   return (
-    <Stack.Navigator>
-      <Stack.Screen name="Home" component={Screen1} />
-      <Stack.Screen name="Profile" component={Screen2} />
+    <Stack.Navigator screenOptions={{headerShown:false}}>
+      <Stack.Screen name="Screen1" component={Screen1} />
+      <Stack.Screen name="Screen2" component={Screen2} />
     </Stack.Navigator>
   );
 }
