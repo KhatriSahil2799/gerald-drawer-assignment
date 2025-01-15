a32dacff2cefd70cdd14bdece5b97d57a283ba49
KhatriSahil2799 <khatrisahil2799@gmail.com>
2025-01-16 01:06:26 +0530

    Implemented Screen1 & screen2

--- src/screens/Screen1.tsx
+++ src/screens/Screen1.tsx
@@ -1,25 +1,53 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import React from "react";
 import Header from "../components/Header";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
-import { NavigationProp, useNavigation } from "@react-navigation/native";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { AppNavigationProp, HomeStackParamList, RootStackParamList } from "../utils/types";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigationProp } from "../utils/types";
 
- 
-
-const Screen1 = ( ) => {
+const Screen1 = () => {
   const { top } = useSafeAreaInsets();
   const navigation = useNavigation<AppNavigationProp>();
- 
+
+  const onPress = () =>
+    navigation.navigate("BottomTabStack", {
+      screen: "Home",
+      params: {
+        screen: "Screen2",
+      },
+    });
+
   return (
-    <>
-      <View style={{ flex: 1, backgroundColor: "white", paddingTop: top }}>
-        <Header />
-        <Text onPress={() => navigation.jumpTo}>Screen1</Text>
+    <View style={[styles.container, { paddingTop: top }]}>
+      <Header title="Screen 1" />
+      <View style={styles.content}>
+        <Pressable style={styles.button} onPress={onPress}>
+          <Text style={styles.text}>Go to Screen 2</Text>
+        </Pressable>
       </View>
-    </>
+    </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+  },
+  content: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    padding:8,
+    paddingHorizontal:16,
+    borderRadius:16,
+    backgroundColor:'#0000FFAA'
+  },
+  text:{
+    color:'#fff'
+  }
+});
 
 export default Screen1;
--- src/screens/Screen2.tsx
+++ src/screens/Screen2.tsx
@@ -1,12 +1,45 @@
-import { View, Text } from "react-native";
+import { View, Text, StyleSheet, Pressable } from "react-native";
 import React from "react";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
+import Header from "../components/Header";
+import { useNavigation } from "@react-navigation/native";
+import { AppNavigationProp } from "../utils/types";
 
 const Screen2 = () => {
+  const { top } = useSafeAreaInsets();
+  const navigation = useNavigation<AppNavigationProp>();
+  
   return (
-    <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
-      <Text>Screen2</Text>
+    <View style={[styles.container, { paddingTop: top }]}>
+      <Header title="Screen 2" />
+      <View style={styles.content}>
+        <Pressable style={styles.button} onPress={navigation.goBack}>
+          <Text style={styles.text}>Go to Screen 1</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#FFF",
+  },
+  content: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  button: {
+    padding: 8,
+    paddingHorizontal: 16,
+    borderRadius: 16,
+    backgroundColor: "#0000FFAA",
+  },
+  text: {
+    color: "#fff",
+  },
+});
 
 export default Screen2;
