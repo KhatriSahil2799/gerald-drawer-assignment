ee5dda82a30040ac17b345c0867158d48343fa63
KhatriSahil2799 <khatrisahil2799@gmail.com>
2025-01-15 15:52:55 +0530

    Implemented Drawer structure

--- src/navigation/DrawerStack.tsx
+++ src/navigation/DrawerStack.tsx
@@ -1,34 +1,71 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-import { createDrawerNavigator } from "@react-navigation/drawer";
+import {
+  createDrawerNavigator,
+  useDrawerProgress,
+} from "@react-navigation/drawer";
 import TabStack from "./TabStack";
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+} from "react-native-reanimated";
 
 const Drawer = createDrawerNavigator();
 
 function DrawerStack() {
   return (
     <Drawer.Navigator
-      drawerContent={(props) => {
-        return (
-          <View
-            style={{
-              flex: 1,
-              backgroundColor: "red",
-              justifyContent: "center",
-            }}
-          >
-            <Text>Drawer</Text>
-          </View>
-        );
-      }}
+      drawerContent={DrawerComponent}
       screenOptions={{
         drawerType: "front",
+        overlayColor: "transparent",
+        headerShown: false,
+        drawerStyle: {
+          width: 180,
+        },
       }}
     >
       <Drawer.Screen name="TabStack" component={TabStack} />
     </Drawer.Navigator>
   );
 }
+
+const AnimatedDrawerComponet = () => {
+  const progress = useDrawerProgress();
+
+  const style = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateY: interpolate(progress.value, [0, 1], [0, 70]) },
+        // {
+        //   rotateZ: `${interpolate(progress.value, [0, 1], [0, -15])}deg`,
+        // },
+      ],
+    };
+  });
+
+  return (
+    <Animated.View style={[{ flex: 1 }, style]}>
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: "red",
+          justifyContent: "center",
+        }}
+      >
+        <Text>Drawer</Text>
+      </View>
+    </Animated.View>
+  );
+};
+
+const DrawerComponent = (props) => {
+  return (
+    // <DrawerContentScrollView>
+    <AnimatedDrawerComponet />
+    // </DrawerContentScrollView>
+  );
+};
 
 export default DrawerStack;
--- src/navigation/TabStack.tsx
+++ src/navigation/TabStack.tsx
@@ -1,20 +1,41 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import React from "react";
 
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import HomeStack from './HomeStack';
-import Contact from '../screens/Contact';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HomeStack from "./HomeStack";
+import Contact from "../screens/Contact";
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import { useDrawerProgress } from "@react-navigation/drawer";
 
 const Tab = createBottomTabNavigator();
 
 function BottomTabStack() {
+  const progress = useDrawerProgress();
+  console.log("🚀 ~ BottomTabStack ~ progress:", progress.value);
+
+  const style = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: interpolate(progress.value, [0, 1], [0, 300]) },
+        {
+          rotateZ: `${interpolate(progress.value, [0, 1], [0, -15])}deg`,
+        },
+        { translateY: interpolate(progress.value, [0, 1], [0, 70]) },
+      ],
+    };
+  });
+
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeStack} />
-      <Tab.Screen name="Contact" component={Contact} />
-    </Tab.Navigator>
+    <Animated.View style={[style, { flex: 1 }]}>
+      <Tab.Navigator >
+        <Tab.Screen name="Home" component={HomeStack} />
+        <Tab.Screen name="Contact" component={Contact} />
+      </Tab.Navigator>
+    </Animated.View>
   );
 }
- 
 
-export default BottomTabStack
+export default BottomTabStack;
